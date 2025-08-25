86656565c17f62b3957e4bea5504dcf876bda76a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-25 10:26:18 +0000

    Aug 25, 2025, 10:26 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,11 +1,11 @@
 let colorValue = 0;
-let xpos = 50;
-let ypos = 50;
 let dia = 25
 function setup() {
     createCanvas(600,600);
 }
 function draw() {
+let xpos = 50;
+let ypos = 50;
 for (let i = 1; i < 11; i++) {
 fill(0,colorValue,0);
 circle(xpos,ypos, dia);
