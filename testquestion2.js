ed96ed43c78705b95645ab9c04576cb7628a5b57
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-25 10:18:24 +0000

    Aug 25, 2025, 10:18 AM

--- testquestion2.js
+++ testquestion2.js
@@ -7,7 +7,7 @@ ypos = 50;
 function draw() {
 for (let i = 1; i < 9; i++) {
 circle(xpos,ypos,55);
-fill(colorValue);
+fill(0,colorValue,0);
 xpos = xpos + 50;
 ypos = ypos + 50;
 colorValue = colorValue + 40
