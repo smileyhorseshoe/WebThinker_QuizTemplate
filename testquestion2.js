36d582dfe8c5d40c7990036c58fab6126c5b8c1c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-25 10:20:29 +0000

    Aug 25, 2025, 10:20 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,8 +2,8 @@ function setup() {
     createCanvas(600,600);
 let colorValue = 0;
 let xpos = 50;
-ypos = 50;
-dia = 25
+let ypos = 50;
+let dia = 25
 }
 function draw() {
 for (let i = 1; i < 9; i++) {
