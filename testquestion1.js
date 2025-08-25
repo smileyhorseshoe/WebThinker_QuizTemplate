e89d8657bdecb18c219e31508a7ea87e9a4d0948
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-25 10:02:21 +0000

    Aug 25, 2025, 10:02 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,7 +6,7 @@ function draw() {
 fill("black");
 stroke("red");
 strokeWeight(10);
-circle(100,200,100)
-triangle(200,250,400,250,300,100)
-rect(400)
+// circle(100,200,100)
+// triangle(200,250,400,250,300,100)
+rect(400,400,50,50)
 }
